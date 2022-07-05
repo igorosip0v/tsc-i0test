210dff01be677076c2905f0f7fc9c6a85df3febf
Igor Osipov <igor.osipov@ottofeller.com>
2022-07-05 18:44:26 +0300

    simple component rollup

--- src/components/SayHello.tsx
+++ src/components/SayHello.tsx
@@ -1,8 +1,5 @@
 import { JSX } from "preact/jsx-runtime";
-import { useState } from "preact/hooks";
 
 export const SayHello = (props: { name: string }): JSX.Element => {
-  const [count, _setCount] = useState(1);
-
-  return <div>{`Hello ${props.name} - ${count}`}</div>;
+  return <div>{`Hello ${props.name}`}</div>;
 };
